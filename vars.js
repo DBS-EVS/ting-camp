479b7ba4061b30fb02c842007796ab02c9b55473
DBS-EVS <104421792+DBS-EVS@users.noreply.github.com>
2022-05-20 13:46:29 +0000

    Deploying to gh-pages from master @ 1e72ead5cd2858a26810c7aed4e5d6dd76117b6a 🚀

--- vars.js
+++ vars.js
@@ -1,4 +1,4 @@
 export var programMsg = "Möchtest du den Sessionplan ansehen?";
-export var urlProgram = "https://db.de/workadventure";
+export var urlProgram = "https://db-planet.deutschebahn.com/pages/network/apps/wiki/ueber-2/list/view/bbed4d6c-ef10-4a61-8e77-8b30c4d7be81?currentLanguage=NONE";
 export var programZone = "program";
 export var programPopUp = "popUpProgram";
